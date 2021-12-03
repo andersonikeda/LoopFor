506c592ec91fb526f96b1e5266fbdb7ac39df271
Anderson <Anderson@DESKTOP-NC5OC9G>
2021-12-03 16:58:19 -0300

    Add project files.

---
+++ LoopFor/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnListar_Click(sender As Object, e As EventArgs) Handles btnListar.Click
+
+        limparLista()
+        Dim i As Integer
+
+        For i = 1 To 5
+            lstLista.Items.Add("O " & i & "º da lista")
+        Next
+
+    End Sub
+
+    Private Sub limparLista()
+        lstLista.Items.Clear()
+    End Sub
+
+    Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
+        limparLista()
+    End Sub
+End Class
